ca9968b19b481b0464b5cb9f4753fc365b328a0c
annopo <pecorium@gmail.com>
2021-02-02 23:02:11 +0900

    get RSS

---
+++ NewsReader/NewsReader/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  NewsReader
+//
+//  Created by pecorium on 2021/02/02.
+//  Copyright © 2021 annopo. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var link = ""
+}
