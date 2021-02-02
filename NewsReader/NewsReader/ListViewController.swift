4091f68ab00377071f658f4bf1ddb77fc043cd78
annopo <pecorium@gmail.com>
2021-02-02 22:06:24 +0900

    table view

---
+++ NewsReader/NewsReader/ListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ListViewController.swift
+//  NewsReader
+//
+//  Created by pecorium on 2021/02/02.
+//  Copyright © 2021 annopo. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+}
