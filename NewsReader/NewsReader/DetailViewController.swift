740331de4025734a9009993c394d9773afa37e2e
annopo <pecorium@gmail.com>
2021-02-03 14:43:47 +0900

    navigation controller

---
+++ NewsReader/NewsReader/DetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DetailViewController.swift
+//  NewsReader
+//
+//  Created by pecorium on 2021/02/03.
+//  Copyright © 2021 annopo. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+    
+    var link:String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let url = URL(string: self.link) {
+            let request = URLRequest(url: url)
+            self.webView.load(request)
+        }
+    }
+}
