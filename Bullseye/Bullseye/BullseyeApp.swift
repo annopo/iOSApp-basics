0a0cbb158d147240225be7966add18d6bc7611d6
annopo <pecorium@gmail.com>
2021-02-08 13:48:45 +0900

    create bullseye slider

---
+++ Bullseye/Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by pecorium on 2021/02/08.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
