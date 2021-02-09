2636d271dc950b47eb2277ba1d593ab7313c943f
annopo <pecorium@gmail.com>
2021-02-09 19:33:19 +0900

    made the score return

---
+++ Bullseye/Bullseye/Models/Game.swift
@@ -0,0 +1,18 @@
+//
+//  Game.swift
+//  Bullseye
+//
+//  Created by pecorium on 2021/02/09.
+//
+
+import Foundation
+
+struct Game {
+    var target: Int = 42
+    var score: Int = 0
+    var round: Int = 1
+    
+    func points(sliderValue: Int) -> Int {
+        return 999
+    }
+}
