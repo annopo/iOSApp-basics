f5fad181212cee47210fcb80e5d53e8d6a58d3a8
annopo <pecorium@gmail.com>
2021-02-10 18:12:37 +0900

    add unit test

---
+++ Bullseye/BullseyeTests/BullseyeTests.swift
@@ -0,0 +1,27 @@
+//
+//  BullseyeTests.swift
+//  BullseyeTests
+//
+//  Created by pecorium on 2021/02/10.
+//
+
+import XCTest
+@testable import Bullseye
+
+class BullseyeTests: XCTestCase {
+    
+    var game: Game!
+    
+    override func setUpWithError() throws {
+        game = Game()
+    }
+    
+    override func tearDownWithError() throws {
+        game = nil
+    }
+    
+    func testExample() throws {
+        XCTAssertEqual(game.points(sliderValue: 50), 999)
+    }
+    
+}
