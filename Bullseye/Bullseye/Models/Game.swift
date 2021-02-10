cc9b420c16bfb10db6ceb1d44b4ba695c405106b
annopo <pecorium@gmail.com>
2021-02-10 19:05:35 +0900

    changed to express the difference between slider value and target as the absolute values

--- Bullseye/Bullseye/Models/Game.swift
+++ Bullseye/Bullseye/Models/Game.swift
@@ -13,14 +13,7 @@ struct Game {
     var round: Int = 1
     
     func points(sliderValue: Int) -> Int {
-        var difference: Int
-        if sliderValue > self.target {
-            difference = sliderValue - self.target
-        } else if self.target > sliderValue {
-            difference = self.target - sliderValue
-        } else {
-            difference = 0
-        }
+        var difference: Int = abs(self.target - sliderValue)
         var awardedPoints: Int = 100 - difference
         return awardedPoints
     }
