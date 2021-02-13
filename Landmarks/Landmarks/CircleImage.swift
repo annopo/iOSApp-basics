77b441f64a0a1ce9e8b9e49ec840b81085a89ca5
annopo <pecorium@gmail.com>
2021-02-13 15:45:53 +0900

    composed the detail view

--- Landmarks/Landmarks/CircleImage.swift
+++ Landmarks/Landmarks/CircleImage.swift
@@ -11,8 +11,8 @@ struct CircleImage: View {
     var body: some View {
         Image("turtlerock")
             .clipShape(Circle())
-            .overlay(Circle().stroke(Color.gray, lineWidth: 4))
-            .shadow(radius: 7)
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 6)
     }
 }
 
