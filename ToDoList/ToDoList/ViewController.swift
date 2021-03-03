0924bb5ee48f73edd97ffa15eb2377f7e788ac50
annopo <pecorium@gmail.com>
2021-03-03 15:16:45 +0900

    made layout for the todo app

---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by pecorium on 2021/03/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addBtnAction(_ sender: Any) {
+    }
+    
+}
+
