fca65c4f7acf9609376fa72a514f08acbd2974a9
annopo <pecorium@gmail.com>
2021-03-11 13:16:52 +0900

    added weather logo and temperature

--- SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
@@ -19,6 +19,18 @@ struct ContentView: View {
                     .font(.system(size: 32, weight: .medium, design: .default))
                     .foregroundColor(.white)
                     .padding()
+                
+                VStack(spacing: 10){
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)   // マルチカラーになる
+                        .resizable()
+                        .aspectRatio(contentMode: .fit) // アスペクト比をキープする
+                        .frame(width: 180, height: 180)
+                    
+                    Text("76°") // option+shift+8 -> °
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }
                 Spacer()
             }
         }
