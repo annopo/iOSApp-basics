6db71db0e9b9672ea9c9ee1393f8598a3855a875
annopo <pecorium@gmail.com>
2021-03-12 23:08:11 +0900

    created and combined views

--- Landmarks/Landmarks/CircleImage.swift
+++ Landmarks/Landmarks/CircleImage.swift
@@ -2,24 +2,22 @@
 //  CircleImage.swift
 //  Landmarks
 //
-//  Created by pecorium on 2021/02/12.
+//  Created by pecorium on 2021/03/12.
 //
 
 import SwiftUI
 
 struct CircleImage: View {
-    var image: Image
-    
     var body: some View {
-        image
+        Image("turtlerock")
             .clipShape(Circle())
             .overlay(Circle().stroke(Color.white, lineWidth: 4))
-            .shadow(radius: 6)
+            .shadow(radius: 7)
     }
 }
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage(image: Image("turtlerock"))
+        CircleImage()
     }
 }
--- Landmarks/Landmarks/LandmarksApp.swift
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -2,19 +2,16 @@
 //  LandmarksApp.swift
 //  Landmarks
 //
-//  Created by pecorium on 2021/02/11.
+//  Created by pecorium on 2021/03/12.
 //
 
 import SwiftUI
 
 @main
 struct LandmarksApp: App {
-    @StateObject private var modelData = ModelData()
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(modelData)
         }
     }
 }
