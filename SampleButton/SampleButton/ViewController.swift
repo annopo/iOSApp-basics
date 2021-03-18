547c22489417add393ccbb0c6d9374fa8572165e
annopo <pecorium@gmail.com>
2021-03-18 22:26:38 +0900

    create nakigoe app

---
+++ SampleButton/SampleButton/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  SampleButton
+//
+//  Created by pecorium on 2021/03/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func dogButton(_ sender: Any) {
+        label.text = "ワンワン"
+    }
+    
+    @IBAction func catButton(_ sender: Any) {
+        label.text = "ニャーニャー"
+    }
+    
+    @IBAction func frogButton(_ sender: Any) {
+        label.text = "ゲロゲロ"
+    }
+    
+}
+
