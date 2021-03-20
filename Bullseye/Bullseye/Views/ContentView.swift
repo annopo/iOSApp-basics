56d50ff4ce6074522970b5d35002144ad47caf9d
annopo <pecorium@gmail.com>
2021-03-20 16:18:57 +0900

    learned debug

--- Bullseye/Bullseye/Views/ContentView.swift
+++ Bullseye/Bullseye/Views/ContentView.swift
@@ -58,7 +58,6 @@ struct HitMeButton: View {
   
   var body: some View {
     Button(action: {
-      print("Hello, SwiftUI!")
       alertIsVisible = true
     }) {
       Text("Hit me".uppercased())
@@ -70,7 +69,8 @@ struct HitMeButton: View {
       ZStack {
         Color("ButtonColor")
         LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
-      })
+      }
+    )
     .foregroundColor(Color.white)
     .cornerRadius(21.0)
     .overlay(
@@ -79,7 +79,10 @@ struct HitMeButton: View {
     )
     .alert(isPresented: $alertIsVisible, content: {
       let roundedValue = Int(sliderValue.rounded())
-      return Alert(title: Text("Hello, there!"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round."), dismissButton: .default(Text("Awesome!")))
+      let points = game.points(sliderValue: roundedValue)
+      return Alert(title: Text("Hello, there!"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(points  ) points this round."), dismissButton: .default(Text("Awesome!")) {
+          game.startNewRound(points: points)
+      })
     })
   }
 }
