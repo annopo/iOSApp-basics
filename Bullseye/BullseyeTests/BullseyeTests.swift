ab38ceb1fffb8521b9614808b04da3dd7f2e8053
annopo <pecorium@gmail.com>
2021-03-20 15:44:51 +0900

    added test

--- Bullseye/BullseyeTests/BullseyeTests.swift
+++ Bullseye/BullseyeTests/BullseyeTests.swift
@@ -9,27 +9,32 @@ import XCTest
 @testable import Bullseye
 
 class BullseyeTests: XCTestCase {
-
-    var game: Game!
-    
-    override func setUpWithError() throws {
-        game = Game()
-    }
-
-    override func tearDownWithError() throws {
-        game = nil
-    }
-
-    func testScorePositive() {
-        let guess = game.target + 5
-        let score = game.points(sliderValue: guess)
-        XCTAssertEqual(score, 95)   // 第一引数と第二引数が等しいかどうかを調べる
-    }
-    
-    func testScoreNegative() {
-        let guess = game.target - 5
-        let score = game.points(sliderValue: guess)
-        XCTAssertEqual(score, 95)   // 第一引数と第二引数が等しいかどうかを調べる
-    }
-
+  
+  var game: Game!
+  
+  override func setUpWithError() throws {
+    game = Game()
+  }
+  
+  override func tearDownWithError() throws {
+    game = nil
+  }
+  
+  func testScorePositive() {
+    let guess = game.target + 5
+    let score = game.points(sliderValue: guess)
+    XCTAssertEqual(score, 95)   // 第一引数と第二引数が等しいかどうかを調べる
+  }
+  
+  func testScoreNegative() {
+    let guess = game.target - 5
+    let score = game.points(sliderValue: guess)
+    XCTAssertEqual(score, 95)   // 第一引数と第二引数が等しいかどうかを調べる
+  }
+  
+  func testNewRound() {
+    game.startNewRound(points: 100)
+    XCTAssertEqual(game.score, 100)
+    XCTAssertEqual(game.round, 2)
+  }
 }
