d6a37e9f34985dee9c5ac24ea6318c5317b55e72
annopo <pecorium@gmail.com>
2021-03-24 18:13:55 +0900

    learned iterating collections

---
+++ IteratingCollections.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+let daysOfTheWeek: [String] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let poolTemperature: [Int] = [78, 81, 74, 80, 79, 83, 84]
+
+for i in 0..<daysOfTheWeek.count where poolTemperature[i] >= 80{
+  print("\(daysOfTheWeek[i]): \(poolTemperature[i])")
+}
+
+var sum = 0
+for temperature in poolTemperature {
+  sum += temperature
+}
+sum / poolTemperature.count
