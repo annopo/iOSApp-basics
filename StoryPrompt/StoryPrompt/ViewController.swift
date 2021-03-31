d86d203ab4c495f92cac4ed6d942ed19ceb873c5
annopo <pecorium@gmail.com>
2021-03-31 16:06:09 +0900

    dismiss the keyboard

--- StoryPrompt/StoryPrompt/ViewController.swift
+++ StoryPrompt/StoryPrompt/ViewController.swift
@@ -9,16 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var nounTextField: UITextField!
+  @IBOutlet weak var adjectiveTextField: UITextField!
+  @IBOutlet weak var verbTextField: UITextField!
+  @IBOutlet weak var numberSlider: UISlider!
+  @IBOutlet weak var numberLabel: UILabel!
+  
+  let storyPrompt = StoryPromptEntry()
+  
+  @IBAction func changeNumber(_ sender: UISlider) {
+    numberLabel.text = "Number: \(Int(sender.value))"
+    storyPrompt.number = Int(sender.value)
+  }
+  
+  @IBAction func changeStoryType(_ sender: UISegmentedControl) {
+    if let genre = StoryPrompts.Genre(rawValue: sender.selectedSegmentIndex) {
+      storyPrompt.genre = genre
+    } else {
+      storyPrompt.genre = .scifi
+    }
+  }
+  @IBAction func generateStoryPrompt(_ sender: Any) {
+    updateStoryPrompt()
+    print(storyPrompt)
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    let storyPrompt = StoryPromptEntry()
+    
+    numberSlider.value = 7.5
     storyPrompt.noun = "toaster"
     storyPrompt.adjective = "smelly"
     storyPrompt.verb = "burps"
-    storyPrompt.number = 10
-    print(storyPrompt)
+    storyPrompt.number = Int(numberSlider.value)
   }
-
-
+  
+  func updateStoryPrompt() {
+    storyPrompt.noun = nounTextField.text ?? ""
+    storyPrompt.adjective = adjectiveTextField.text ?? ""
+    storyPrompt.verb = verbTextField.text ?? ""
+  }
 }
 
+extension ViewController: UITextFieldDelegate {
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    updateStoryPrompt()
+    return true
+  }
+}
