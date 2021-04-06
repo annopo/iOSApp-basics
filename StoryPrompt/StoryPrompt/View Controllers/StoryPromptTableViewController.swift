80924f9c8cca02800885669b8b86469dc5144c13
annopo <pecorium@gmail.com>
2021-04-06 16:05:21 +0900

    add table view

---
+++ StoryPrompt/StoryPrompt/View Controllers/StoryPromptTableViewController.swift
@@ -0,0 +1,56 @@
+//
+//  StoryPromptTableViewController.swift
+//  StoryPrompt
+//
+//  Created by pecorium on 2021/04/06.
+//
+
+import UIKit
+
+class StoryPromptTableViewController: UITableViewController {
+  
+  var storyPrompts = [StoryPromptEntry]()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    let storyPrompt1 = StoryPromptEntry()
+    let storyPrompt2 = StoryPromptEntry()
+    let storyPrompt3 = StoryPromptEntry()
+    
+    storyPrompt1.noun = "toaster"
+    storyPrompt1.adjective = "smelly"
+    storyPrompt1.verb = "attacks"
+    storyPrompt1.number = 5
+    
+    storyPrompt2.noun = "toaster"
+    storyPrompt2.adjective = "smelly"
+    storyPrompt2.verb = "attacks"
+    storyPrompt2.number = 5
+    
+    storyPrompt3.noun = "toaster"
+    storyPrompt3.adjective = "smelly"
+    storyPrompt3.verb = "attacks"
+    storyPrompt3.number = 5
+    
+    storyPrompts = [storyPrompt1, storyPrompt2, storyPrompt3]
+  }
+  
+  // MARK: - Table view data source
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    1
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // #warning Incomplete implementation, return the number of rows
+    storyPrompts.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "StoryPromptCell", for: indexPath)
+    cell.textLabel?.text = "Prompt \(indexPath.row + 1)"
+    cell.imageView?.image = storyPrompts[indexPath.row].image
+    return cell
+  }
+}
