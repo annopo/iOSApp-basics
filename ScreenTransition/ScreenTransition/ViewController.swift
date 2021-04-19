439dc23ff986c515e84a411245c29ebb7932034c
annopo <pecorium@gmail.com>
2021-04-19 22:51:36 +0900

    push transition and modal transition

---
+++ ScreenTransition/ScreenTransition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenTransition
+//
+//  Created by pecorium on 2021/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
