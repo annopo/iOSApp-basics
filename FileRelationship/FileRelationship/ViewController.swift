c6d98e9abff88b054657625e2a48e069204b18ee
annopo <pecorium@gmail.com>
2021-05-02 18:30:26 +0900

    added fileRelationship

---
+++ FileRelationship/FileRelationship/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FileRelationship
+//
+//  Created by pecorium on 2021/05/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  @IBOutlet weak var label1: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    label1.text = "1つ目の画面"
+  }
+
+
+}
+
