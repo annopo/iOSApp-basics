a2bf9deb091adbd310561310b07e1de4a0bc681e
annopo <anna.ishizaki@hey.jp>
2021-05-25 18:53:36 +0900

    create A or B app

---
+++ AorB/AorB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AorB
+//
+//  Created by anna.ishizaki on 2021/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
